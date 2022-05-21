ed5713092b4b6df46daff2bf1d8c8674e79675b9
victor099 <victor.pereira@shipay.com.br>
2022-05-21 17:49:47 -0300

    feat(#2): addding listing methods in facade

--- duello/cage/facade.py
+++ duello/cage/facade.py
@@ -1,5 +1,7 @@
 from duello.custom_auth.models import Users
+from duello.cage.models import Cage
 from duello.cage.serializers.cage_serializer import CageSerializer
+from django.shortcuts import  get_object_or_404
 
 
 def create_cage(creator, title, description):
@@ -8,4 +10,15 @@ def create_cage(creator, title, description):
     serializer.is_valid()
     serializer.save()
     return serializer.data
+
+
+def retrieve_cage_by_id(id):
+   cage = get_object_or_404(Cage, pk=id)
+   serializer = CageSerializer(cage)
+   return serializer.data
+
+
+def list_cages():
+    queryset = CageSerializer(Cage.objects.all(), many=True)
+    return queryset.data
 
