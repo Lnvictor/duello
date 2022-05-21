3b3a4f0b6797dd0105c490324dc64008f507c608
victor099 <victor.pereira@shipay.com.br>
2022-05-21 15:39:49 -0300

    feat(#2): addding route mappings

--- duello/cage/urls.py
+++ duello/cage/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 
 from .views import CageViewSet
 
-urlpatterns = [path("cage", CageViewSet.as_view({"post": "create"}), name="cage")]
+urlpatterns = [
+    path("cage", 
+        CageViewSet.as_view({"post": "create", "get": "list"}), 
+         name="cage"), 
+    path("cage/<int:id>", CageViewSet.as_view({"get":"retrieve", "delete": "destroy"}), name="cage_by_id")
+]
