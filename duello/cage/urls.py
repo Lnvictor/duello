851b0c5f1f01166996ada4b74f376c2a0edd0be8
victor099 <victor.pereira@shipay.com.br>
2022-05-22 15:22:03 -0300

    fix(#2): lint adjusts

--- duello/cage/urls.py
+++ duello/cage/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from .views import CageViewSet
 
 urlpatterns = [
-    path("cage", 
-        CageViewSet.as_view({"post": "create", "get": "list"}), 
-         name="cage"), 
-    path("cage/<int:id>", CageViewSet.as_view({"get":"retrieve", "delete": "destroy", "put": "update"}), name="cage_by_id")
+    path("cage", CageViewSet.as_view({"post": "create", "get": "list"}), name="cage"),
+    path(
+        "cage/<int:id>",
+        CageViewSet.as_view({"get": "retrieve", "delete": "destroy", "put": "update"}),
+        name="cage_by_id",
+    ),
 ]
