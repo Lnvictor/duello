259dc31a7c73691df9be496a5e603fa327685767
victor099 <victor.pereira@shipay.com.br>
2022-05-21 22:45:43 -0300

    feat(#1): adding CageViewSet

--- duello/cage/views.py
+++ duello/cage/views.py
@@ -5,16 +5,26 @@ from rest_framework.response import Response
 
 from duello.cage.serializers.cage_serializer import CageSerializer
 from duello.custom_auth.facade import JwtAuthentication
+from duello.cage.facade import create_cage, retrieve_cage_by_id, list_cages, delete_cage, update_cage
 
 
 @authentication_classes([JwtAuthentication])
 @permission_classes([IsAuthenticated])
 class CageViewSet(viewsets.ViewSet):
     def create(self, request):
-        serializer = CageSerializer(data=request.data)
-        serializer.is_valid()
-        serializer.save()
-        return Response(serializer.data)
+        return Response(create_cage(**request.data))
 
-    def get_permission(self):
-        return [IsAuthenticated()]
+    def update(self, request, id):
+        data = request.data
+        return Response(update_cage(id, data))
+
+    def list(self, request):
+        return Response(list_cages())
+
+    def retrieve(self, request, id):
+        return Response(retrieve_cage_by_id(id))
+
+    def destroy(self, request, id):
+        delete_cage(id)
+        return Response(status=200)
+
