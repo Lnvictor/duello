b8f6d6ca8430c29e1f134082c5b36ac6e0328bda
victor099 <victor.pereira@shipay.com.br>
2022-05-22 14:55:37 -0300

    feat(#1): adding tests for retrieving methods

--- duello/cage/tests/test_cage_services.py
+++ duello/cage/tests/test_cage_services.py
@@ -1,11 +1,10 @@
 import pytest
 from django.core.exceptions import ObjectDoesNotExist
 
-from duello.cage.facade import create_cage
+from duello.cage.facade import create_cage, list_cages, retrieve_cage_by_id, update_cage, delete_cage
 
 
 def test_should_not_create_cage_due_to_invalid_creator(mocker, user_mock, cage_mock):
-    # TODO: Test exception that must be raised when a invalid user_id is sended (Use pytest.raises)
     with pytest.raises(ObjectDoesNotExist):
         mocker.patch("duello.cage.facade.Users.objects", return_value=user_mock)
         mocker.patch(
@@ -22,10 +21,23 @@ def test_should_not_create_cage_due_to_invalid_creator(mocker, user_mock, cage_m
 
 
 def test_shoud_create_cage(mocker, user_mock, cage_mock):
-    # TODO: Test happy path
     mocker.patch("duello.cage.facade.Users.objects", return_value=user_mock)
     mocker.patch('duello.cage.facade.CageSerializer.save', return_value=True)
     mocker.patch("duello.cage.facade.Cage", return_value=cage_mock)
     assert create_cage(
         creator=user_mock, title=cage_mock.title, description=cage_mock.description
     )
+
+
+def test_should_retrieve_all_cages(mocker, cage_mock):
+    mocker.patch('duello.cage.facade.Cage.objects.all', return_value=[cage_mock, cage_mock])
+    cages = list_cages()
+    assert cages[0].get('title') == cage_mock.title
+
+
+def test_should_retrieve_cage_by_id(mocker, cage_mock):
+    mocker.patch('duello.cage.facade.get_object_or_404', return_value=cage_mock)
+    cage = retrieve_cage_by_id(cage_mock.id)
+
+    assert cage.get('title') == cage_mock.title
+
