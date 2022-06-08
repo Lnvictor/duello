22aa28cf11464026126648d433638af6cc6000fb
victor099 <victor.pereira@shipay.com.br>
2022-06-07 23:38:36 -0300

    feat: adding answer service

--- duello/cage/facade.py
+++ duello/cage/facade.py
@@ -2,8 +2,8 @@ from django.db.models import Model
 from django.shortcuts import get_object_or_404
 from rest_framework.serializers import ModelSerializer
 
-from duello.cage.models import Cage, Question
-from duello.cage.serializers.cage_serializer import CageSerializer, QuestionSerializer
+from duello.cage.models import Cage, Question, Answer
+from duello.cage.serializers import CageSerializer, QuestionSerializer, AnswerSerializer
 from duello.custom_auth.models import Users
 
 
@@ -13,13 +13,24 @@ class BaseModelService:
 
     def __init__(self):
         self._serializer = self.serializer_type()
-        self._list_serializer = self.serializer_type(many=True)
 
-    def create(self, data):
-        if self._serializer is not None:
+    def _renew_serializer(self, instance=None, data=None):
+        if self._serializer:
             del(self._serializer)
 
-        self._serializer = self.serializer_type(data=data)
+        if instance is not None:
+            self._serializer = self.serializer_type(instance=instance)
+            return
+        
+        if data is not None:
+            self._serializer = self.serializer_type(data=data)
+            return
+
+        raise Exception('You must pass either data or instance fields')
+
+
+    def create(self, data):
+        self._renew_serializer(data=data)
         self._serializer.is_valid()
         cage = self._serializer.save()
         return self._serializer.data
@@ -27,19 +38,20 @@ class BaseModelService:
     
     def retrieve_by_id(self, id):
         obj = get_object_or_404(self.model_type, pk=id)
-        self._serializer.instance = obj
+        self._renew_serializer(instance=obj)
         return self._serializer.data
     
     def list(self):
-        self._list_serializer.instance = self.model_type.objects.all()
-        return self._list_serializer.data
-    
+        obj_list = []
+        for obj in self.model_type.objects.all():
+            self._renew_serializer(instance=obj)
+            obj_list.append(self._serializer.data)
+
+        return obj_list
     
     def update(self, id, data):
-        obj = get_object_or_404(Cage, pk=id)
-        if self._serializer is not None:
-            del(self._serializer)
-        self._serializer = self.serializer_type(data=data)
+        obj = get_object_or_404(self.model_type, pk=id)
+        self._renew_serializer(data=data) 
         self._serializer.is_valid()
         self._serializer.update(obj, data)
         return self._serializer.data
@@ -62,6 +74,11 @@ class QuestionService(BaseModelService):
     serializer_type = QuestionSerializer
 
 
+class AnswerService(BaseModelService):
+    model_type = Answer
+    serializer_type = AnswerSerializer
+
 # Singleton
 cage_service = CageService()
 question_service = QuestionService()
+answer_service = AnswerService()
