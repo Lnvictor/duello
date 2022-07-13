3b236187783fbfa901a9ef3ef19a74738f2b1090
Lnvictor <Lnvictor@github.com>
2022-07-12 22:16:24 -0300

    feat: refactoring filter_by method

--- duello/cage/facade.py
+++ duello/cage/facade.py
@@ -21,26 +21,24 @@ class BaseModelService:
         if instance is not None:
             self._serializer = self.serializer_type(instance=instance)
             return
-        
+
         if data is not None:
             self._serializer = self.serializer_type(data=data)
             return
 
         raise Exception('You must pass either data or instance fields')
 
-
     def create(self, data):
         self._renew_serializer(data=data)
         self._serializer.is_valid()
-        cage = self._serializer.save()
+        self._serializer.save()
         return self._serializer.data
-    
-    
+
     def retrieve_by_id(self, id):
         obj = get_object_or_404(self.model_type, pk=id)
         self._renew_serializer(instance=obj)
         return self._serializer.data
-    
+
     def list(self):
         obj_list = []
         for obj in self.model_type.objects.all():
@@ -48,39 +46,39 @@ class BaseModelService:
             obj_list.append(self._serializer.data)
 
         return obj_list
-    
+
     def update(self, id, data):
         obj = get_object_or_404(self.model_type, pk=id)
-        self._renew_serializer(data=data) 
+        self._renew_serializer(data=data)
         self._serializer.is_valid()
         self._serializer.update(obj, data)
         return self._serializer.data
-    
-    
+
     def delete(self, id):
         obj = get_object_or_404(self.model_type, pk=id)
         obj.delete()
         obj.save()
         return obj
 
+    def filter_by(self, filters):
+        model_response = {}
+
+        for pair in filters.items():
+            tmp_filter = {pair[0]: pair[1]}
+            objects = self.model_type.objects.filter(**tmp_filter)
+            model_response[pair[0]] = []
+
+            for obj in objects:
+                self._renew_serializer(instance=obj)
+                model_response[pair[0]].append(self._serializer.data)
+
+        return model_response
+
 
 class CageService(BaseModelService):
     model_type = Cage
     serializer_type = CageSerializer
 
-    def filter_cages(self, filters):
-        cages_response = {}
-
-        for pair in filters.items():
-            tmp_filter = {pair[0]: pair[1]}
-            cages = self.model_type.objects.filter(**tmp_filter)
-            cages_response[pair[0]] = []
-
-            for cage in cages:
-                self._renew_serializer(instance=cage)
-                cages_response[pair[0]].append(self._serializer.data)
-
-        return cages_response
 
 class QuestionService(BaseModelService):
     model_type = Question
@@ -90,6 +88,7 @@ class QuestionService(BaseModelService):
 class AnswerService(BaseModelService):
     model_type = Answer
     serializer_type = AnswerSerializer
+
 
 # Singleton
 cage_service = CageService()
